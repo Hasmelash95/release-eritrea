d738275c9d59feb199cb69c69d15dd88e3bf1411
Hasmelash95 <hermonasmelash@yahoo.com>
2022-11-17 14:59:45 +0000

    Add a decorator for simplicity

--- press/admin.py
+++ press/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Article
 from django_summernote.admin import SummernoteModelAdmin
 
+
+@admin.register(Article)
 class ArticleAdmin(SummernoteModelAdmin):
     summernote_fields = ('content')
 
-admin.site.register(Article)
+

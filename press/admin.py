a14634848bf66b1d64073a579582363ac1dff105
Hasmelash95 <hermonasmelash@yahoo.com>
2022-11-17 14:50:38 +0000

    Install summernote to use for content field

--- press/admin.py
+++ press/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Article
+from django_summernote.admin import SummernoteModelAdmin
+
+class ArticleAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
 
 admin.site.register(Article)

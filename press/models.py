5ac536ded40a6f5a198ba03ea48e8aee1e42c123
Hasmelash95 <hermonasmelash@yahoo.com>
2022-11-17 14:31:31 +0000

    Create Article model

--- press/models.py
+++ press/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=300, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ManyToManyField(User, related_name='press_releases')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    

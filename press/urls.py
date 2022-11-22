62dfb18a6a8f92282570cdf1ddc6f4f43b071630
Hasmelash95 <hermonasmelash@yahoo.com>
2022-11-22 18:38:26 +0000

    Display properties of an article on the main page

--- press/urls.py
+++ press/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
     path('', views.ArticleList.as_view(), name='press'),
 ]
--- press/views.py
+++ press/views.py
@@ -4,13 +4,13 @@ from django.views import generic
 from .models import Article
 
 
-def home_page(request):
-    first_template = loader.get_template('index.html')
-    return HttpResponse(first_template.render())
-
-
 class ArticleList(generic.ListView):
     model = Article
     queryset = Article.objects.order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 3
+
+
+def home_page(request):
+    first_template = loader.get_template('index.html')
+    return HttpResponse(first_template.render())

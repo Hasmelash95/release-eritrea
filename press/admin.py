3bd5f4b9801b4370079c888e31bebd07e6a047a2
Hasmelash95 <hermonasmelash@yahoo.com>
2022-11-23 19:14:08 +0000

    Register models for admin site

--- press/admin.py
+++ press/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Article)
 class ArticleAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
     summernote_fields = ('content')
 
 

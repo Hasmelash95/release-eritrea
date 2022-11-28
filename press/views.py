abe9d5d987bfbf1fea691cd55ea0437dcc5f4ccf
Hasmelash95 <hermonasmelash@yahoo.com>
2022-11-28 16:09:41 +0000

    Add views for the article detail page

--- press/views.py
+++ press/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Article
 
 
@@ -9,6 +9,12 @@ class ArticleList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 3
 
+
+class ArticleDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        article = get_object_or_404(queryset, slug=slug)
+        comment = article.comment.filter(approved=True).order_by('created_on')
 
 def home_page(request):
     return render(request, 'index.html')

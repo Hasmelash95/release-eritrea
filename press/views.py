0657f5d24816c5c983d8081ae3e7c55d048388ef
Hasmelash95 <hermonasmelash@yahoo.com>
2022-11-28 19:10:57 +0000

    Add post method for the comment forms

--- press/views.py
+++ press/views.py
@@ -26,6 +26,28 @@ class ArticleDetail(View):
             }
         )
 
+    def post(self, request, slug, *args, **kwargs):
+        article = get_object_or_404(Article, slug=slug)
+        comments = article.comments.filter(approved=True).order_by('created_on')
+        return render(
+            request,
+            "article-detail.html",
+            {
+                "article": article,
+                "comments": comments,
+                "comment_form": CommentForm()     
+            }
+        )  
+
+        comment_form = CommentForm(data=request.POST)  
+        if comment_form.is_valid():
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
 def home_page(request):
     return render(request, 'index.html')
 

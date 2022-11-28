18f2914b4f4f201ad70c8e137307695edaef5795
Hasmelash95 <hermonasmelash@yahoo.com>
2022-11-28 21:30:09 +0000

    Create article form class

--- press/forms.py
+++ press/forms.py
@@ -1,5 +1,10 @@
-from .models import Comment
+from .models import Comment, Article
 from django import forms
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ('title', 'content', 'excerpt')
 
 
 class CommentForm(forms.ModelForm):

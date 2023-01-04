561ef3d5a55aa4063b9e9dabffff12d8abb610eb
Hasmelash95 <hermonasmelash@yahoo.com>
2023-01-04 21:44:53 +0000

    Add fade on scroll effects to index page

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,25 @@
-let today = new Date().getFullYear();
+const scrollFadeIn = document.querySelectorAll('.fade-in');
 const presentYear = document.getElementById('current-year');
 
+let today = new Date().getFullYear();
 presentYear.innerHTML = today;
+
+const fadeOptions = {
+    threshold: 0.3
+};
+
+const visibleOnScroll = new IntersectionObserver(function(
+    entries, visibleOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('scrolled');
+                visibleOnScroll.unobserve(entry.target);
+            }
+        })
+    }, fadeOptions);
+
+scrollFadeIn.forEach(fade => {
+    visibleOnScroll.observe(fade);
+})

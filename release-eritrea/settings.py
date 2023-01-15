40f5257fafc0e696e1dc6581aaa82087e7fb8609
Hasmelash95 <hermonasmelash@yahoo.com>
2023-01-15 19:01:13 +0000

    Add semantic tags to html

--- release-eritrea/settings.py
+++ release-eritrea/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['release-eritrea.herokuapp.com', 'localhost']
 
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'taggit',
     'django_filters',
+    'whitenoise',
     'press',
     'info',
 ]

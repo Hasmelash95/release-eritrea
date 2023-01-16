432eda233cb9a6565b360f7cf77639d4e996b5f1
Hasmelash95 <hermonasmelash@yahoo.com>
2023-01-16 21:00:48 +0000

    Add comments to the js script

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// To get elements from the templates
 const submit = document.getElementsByClassName('submit-btn')[0];
 const peopleIcon = document.getElementById('people')
 const title = document.getElementById('id_title');
@@ -10,8 +11,12 @@ const logMsg = document.getElementsByClassName('log-msg')[0];
 const scrollFadeIn = document.querySelectorAll('.fade-in');
 const presentYear = document.getElementById('current-year');
 
+/**
+ * Event listener to ensure page loads before the functions
+ * are called.
+ */
 document.addEventListener('DOMContentLoaded', function() {
-
+    // Functions below will only be called if the variables are defined
     if (fave != undefined) {
         fave.addEventListener('mouseover', faveHover);
         fave.addEventListener('mouseleave', faveLeave);
@@ -31,26 +36,51 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 })
 
+/**
+ * On hovering over the favorites button on the 
+ * index page, the bullhorn icon on the header changes
+ * to a heart.
+ */
 function faveHover() {
     pressIcon.classList.remove('fa-bullhorn');
     pressIcon.classList.add('fa-heart');
 }
 
+/**
+ * On the mouse leaving the favorites button on the 
+ * index page, the heart icon on the header changes
+ * back to a bullhorn.
+ */
 function faveLeave() {
     pressIcon.classList.remove('fa-heart');
     pressIcon.classList.add('fa-bullhorn');
 }
 
+/**
+ * On hovering over the search button on the 
+ * index page, the bullhorn icon on the header changes
+ * to a magnifying lass.
+ */
 function filterHover() {
     pressIcon.classList.remove('fa-bullhorn');
     pressIcon.classList.add('fa-magnifying-glass');
 }
 
+/**
+ * On the mouse leaving the search button on the 
+ * index page, the magnifying icon on the header changes
+ * back to a bullhorn.
+ */
 function filterLeave() {
     pressIcon.classList.remove('fa-magnifying-glass');
     pressIcon.classList.add('fa-bullhorn');
 }
 
+/**
+ * When the submit button is clicked, an alert will pop up
+ * alerting the user which inputs on the article form need to be 
+ * filled. 
+ */
 function submitCheck() {
     if (title.value.length == 0) {
         alert('The article needs a title!');
@@ -60,18 +90,34 @@ function submitCheck() {
     }
 }
 
+/**
+ * When the log in, sign up or log out buttons are clicked
+ * a spinner will be revealed with a loading message.
+ */
 function logClick() {
     logMsg.classList.remove('visually-hidden');
     
 }
 
+// Sets the year on the footer to the present year
 let today = new Date().getFullYear();
 presentYear.innerHTML = today;
 
+// Sets how much of the elements need to be visible 
 let fadeOptions = {
     threshold: 0.1
 };
 
+/**
+ * Triggers the animation effect when the selected
+ * elements are visible to the user using an 
+ * intersection observer API.
+ * List of entries and visibleOnScroll (intersection 
+ * observer) variable taken as parameters.
+ * Ends the function if entry is not intersecting.
+ * Returns True if entry is intersecting and stops 
+ * observing that entry after the animation is in effect.
+ */
 let visibleOnScroll = new IntersectionObserver(function(
     entries, visibleOnScroll) {
         entries.forEach(entry => {

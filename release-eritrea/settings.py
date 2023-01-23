40578b1f38c2c6d93980a99a9b5b80ee6bc10686
Hasmelash95 <hermonasmelash@yahoo.com>
2023-01-23 15:24:49 +0000

    Remove commented out code and turn debug off

--- release-eritrea/settings.py
+++ release-eritrea/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['release-eritrea.herokuapp.com', 'localhost']
 
@@ -113,13 +113,6 @@ WSGI_APPLICATION = 'release-eritrea.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))

007702059d46ca9fd6141a5e8f765551a2443c67
Hasmelash95 <hermonasmelash@yahoo.com>
2023-01-25 15:47:27 +0000

    Make url changes for consistency

--- press/urls.py
+++ press/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.PressList.as_view(), name='home'),
-    path('article-filter', views.article_filter, name='filter'),
-    path('favorites', views.favorites, name='favorites'),
-    path('post-article', views.post_article, name='post-article'),
+    path('article-filter/', views.article_filter, name='filter'),
+    path('favorites/', views.favorites, name='favorites'),
+    path('post-article/', views.post_article, name='post-article'),
     path('edit-article/<slug:slug>', views.edit_article, name='edit-article'),
     path('delete/<slug:slug>', views.delete_article, name='delete'),
     path('fave-add/<slug:slug>', views.favorite_article, name='fave-add'),
-    path('<slug:slug>/', views.ArticleDetail.as_view(),
+    path('detail/<slug:slug>', views.ArticleDetail.as_view(),
          name='article-detail'),
 ]

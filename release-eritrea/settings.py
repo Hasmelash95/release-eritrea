c8d7cc674da4eb6c27434bf7f92b59b08cc661c1
Hasmelash95 <hermonasmelash@yahoo.com>
2023-01-25 06:44:17 +0000

    Make changes to the mockup screenshots

--- release-eritrea/settings.py
+++ release-eritrea/settings.py
@@ -23,7 +23,6 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -114,16 +113,9 @@ WSGI_APPLICATION = 'release-eritrea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#      'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

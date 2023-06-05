0a8cd4817d55467c1306efef332f8849f126c6b3
Hasmelash95 <hermonasmelash@yahoo.com>
2023-06-05 13:45:04 +0000

    Update CSS and add boostrap classes

--- release-eritrea/settings.py
+++ release-eritrea/settings.py
@@ -30,9 +30,13 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['release-eritrea.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [
+    'release-eritrea.herokuapp.com', 
+    'localhost', 
+    '8000-hasmelash95-releaseerit-kspyhrnwa8a.ws-eu98.gitpod.io'
+    ]
 
 # Application definition
 
